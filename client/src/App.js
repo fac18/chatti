7e1f1d88fd9ff50fe8ada69cff0c1fbe774c5d99
Gillian <gillian@yeomans.org.uk>
2020-02-05 14:42:23 +0000

    create react app and express server

---
+++ client/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  const makeBackendCall = () => {
+    // Get the passwords and store them in state
+    fetch("/api/test")
+      .then(res => res.json())
+      .then(newData => setData(newData));
+  };
+
+  React.useEffect(() => {
+    makeBackendCall();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{data}</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "/../client/build")));
+
+// Put all API endpoints under '/api'
+app.get("/api/test", (req, res) => {
+  res.json("back end is sending some test data");
+
+  console.log(`the test data was sent`);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Chatti server listening on ${port}`);
