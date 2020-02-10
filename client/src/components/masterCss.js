1aee50b30c6ba7e637874e5d2a9512948943940d
Ayub Mohamud <a.mohamud1@hotmail.com>
2020-02-10 11:38:54 +0000

    add mastercss.js file for styled-components

---
+++ client/src/components/masterCss.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Button = styled.button `
+background: #FFC000;
+box-shadow: 6px 6px 4px rgba(0, 0, 0, 0.25), inset -10px -10px 4px #FF8E00,
+    inset 10px 10px 4px #FF8E00;
+width: 280px;
+min-height: 50px;
+font-family: Lato;
+border-radius: 15px;
+text-align: center;
+
+`
+
+const WhiteText = styled.p`
+  color: #FFFFFF
+  display: inline;
+`;
+
+export {
+Button,
+
+
+
+
+}
