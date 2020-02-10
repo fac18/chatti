034f39b12712da91c96325529f36407f4f282854
Gillian <gillian@yeomans.org.uk>
2020-02-10 12:26:39 +0000

    fix failing test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders home page text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(
+    /Interactive ideas to inspire special moments/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
