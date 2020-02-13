1444814ceeae0dfa5397a7d75e12256ad81df89d
Gillian <gillian@yeomans.org.uk>
2020-02-13 17:40:22 +0000

    add router to progress test

--- client/src/components/Progress/Progress.test.js
+++ client/src/components/Progress/Progress.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Progress from "./Progress";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 test("includes Your Progress header", () => {
-  const { getByText } = render(<Progress />);
-  const headerElement = getByText(
-    /Your Progress/i
+  const history = createMemoryHistory();
+  const { getByText } = render(
+    <Router history={history}>
+      <Progress />
+    </Router>
   );
+  const headerElement = getByText(/Your Progress/i);
   expect(headerElement).toBeInTheDocument();
 });
