9eeeb52b434a32157e620907dd3e1968e81fb812
Gillian <gillian@yeomans.org.uk>
2020-02-14 16:13:14 +0000

    link up navbar pages

--- client/src/components/Register3/Register3.js
+++ client/src/components/Register3/Register3.js
@@ -1,27 +1,32 @@
 import React from "react";
 import Header from "../Header/Header";
-import {InputField, GeneralText, InputSubHeading,FormBox, Error, RecommendationParagraph, FormContainer} from '../masterCss'
-import Button from '../button'
-import { Formik, ErrorMessage, Field,Form } from "formik";
-import InsertUserData from '../../utils/postFormData'
+import {
+  InputField,
+  GeneralText,
+  InputSubHeading,
+  FormBox,
+  Error,
+  RecommendationParagraph,
+  FormContainer
+} from "../masterCss";
+import Button from "../button";
+import { Formik, ErrorMessage, Field, Form } from "formik";
+import InsertUserData from "../../utils/postFormData";
 import postFormData from "../../utils/postFormData";
 
 function Register3({ setReg3 }) {
   return (
     <FormContainer>
-      <RecommendationParagraph >
+      <RecommendationParagraph>
         We recommend that you aim For 10 minutes of chatti activity everyday
       </RecommendationParagraph>
-      <Button
-        type="submit"
-        buttonText={"Accept recommended settings"}
-      ></Button>
+      <Button type="submit" buttonText={"Accept recommended settings"}></Button>
 
       <Formik
         initialValues={{
           how_often: "daily",
-          new_ideas_time: "20:00:00",
-          weekly_goal: 80
+          new_ideas_time: "8pm",
+          weekly_goal: "80 mins"
         }}
         // validate={values => {
         //   const errors={}
@@ -40,66 +45,79 @@ function Register3({ setReg3 }) {
           setReg3(values);
         }}
       >
-        {({isSubmitting}) =>(
+        {({ isSubmitting }) => (
           <Form>
-
-            <InputSubHeading>How often do you want to get new ideas?
-          
-            <InputField as="select" name="how_often">
-               {/* <option name="how_often" type="text">daily</option> */}
-              <option name="how_often" type="text">weekly</option>
-              <option name="how_often" type="text">fortnightly</option>
-              <option name="how_often" type="text">monthly</option>
-
-            </InputField>
-
-          <Error name="how_often" component="div"></Error>
-          </InputSubHeading>
-
-          <InputSubHeading>What time do you want to get new ideas?
-          <InputField as="select" name="new_ideas_time">
-               {/* <option name="new_ideas_time" type="text">8pm</option> */}
-              <option name="new_ideas_time" type="text">7pm</option>
-              <option name="new_ideas_time" type="text">6pm</option>
-              <option name="new_ideas_time" type="text">5pm</option>
-              <option name="new_ideas_time" type="text">4pm</option>
-              <option name="new_ideas_time" type="text">3pm</option>
-              <option name="new_ideas_time" type="text">2pm</option>
-              <option name="new_ideas_time" type="text">1pm</option>
-
-            </InputField>
-            <Error name="new_ideas_time" component="div">Please select time</Error>
+            <InputSubHeading>
+              How often do you want to get new ideas?
+              <InputField as="select" name="how_often">
+                {/* <option name="how_often" type="text">daily</option> */}
+                <option name="how_often" type="text">
+                  weekly
+                </option>
+                <option name="how_often" type="text">
+                  fortnightly
+                </option>
+                <option name="how_often" type="text">
+                  monthly
+                </option>
+              </InputField>
+              <Error name="how_often" component="div"></Error>
             </InputSubHeading>
 
-            <InputSubHeading>What is your weekly goal?
-            <InputField as="select" name="weekly_goal">
-               {/* <option name="weekly_goal" type="text">80</option> */}
-                <option name="weekly_goal" type="text">30 minutes</option>
-                <option name="weekly_goal" type="text">60 minutes</option>
-                <option name="weekly_goal" type="text">100 minutes</option>
+            <InputSubHeading>
+              What time do you want to get new ideas?
+              <InputField as="select" name="new_ideas_time">
+                {/* <option name="new_ideas_time" type="text">8pm</option> */}
+                <option name="new_ideas_time" type="text">
+                  7pm
+                </option>
+                <option name="new_ideas_time" type="text">
+                  6pm
+                </option>
+                <option name="new_ideas_time" type="text">
+                  5pm
+                </option>
+                <option name="new_ideas_time" type="text">
+                  4pm
+                </option>
+                <option name="new_ideas_time" type="text">
+                  3pm
+                </option>
+                <option name="new_ideas_time" type="text">
+                  2pm
+                </option>
+                <option name="new_ideas_time" type="text">
+                  1pm
+                </option>
+              </InputField>
+              <Error name="new_ideas_time" component="div">
+                Please select time
+              </Error>
+            </InputSubHeading>
 
-            </InputField>
-          <Error name="weekly_goal" component="div">Please select weekly goal</Error>
-          </InputSubHeading>
+            <InputSubHeading>
+              What is your weekly goal?
+              <InputField as="select" name="weekly_goal">
+                {/* <option name="weekly_goal" type="text">80</option> */}
+                <option name="weekly_goal" type="text">
+                  30 minutes
+                </option>
+                <option name="weekly_goal" type="text">
+                  60 minutes
+                </option>
+                <option name="weekly_goal" type="text">
+                  100 minutes
+                </option>
+              </InputField>
+              <Error name="weekly_goal" component="div">
+                Please select weekly goal
+              </Error>
+            </InputSubHeading>
 
-          <Button type='submit' type="button" buttonText="Continue"></Button>
-
-
-        </Form>
-)}
-
-
-
-
-
-
-
+            <Button type="submit" type="button" buttonText="Continue"></Button>
+          </Form>
+        )}
       </Formik>
-      
-
-
-      
-      
     </FormContainer>
   );
 }
