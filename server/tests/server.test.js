b5836f6b3716651948c637c7aa8a6d0be5327e5b
Gillian <gillian@yeomans.org.uk>
2020-02-18 15:49:35 +0000

    add min 1 test to server

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -13,6 +13,10 @@ afterAll(done => {
   server.close(done)
 })
 
+test('test suite works', () => {
+  expect(1).toBe(1)
+})
+
 // test("test endpoint path works", () => {
 //   return request.get("/api/test").then(response => {
 //     expect(response.statusCode).toBe(200);
