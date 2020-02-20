f1784c19ef947e934a4390099a1de7568270f13c
Gillian <gillian@yeomans.org.uk>
2020-02-20 20:04:01 +0000

    update styling

--- client/src/components/ActivitySummary/ActivitySummary.style.js
+++ client/src/components/ActivitySummary/ActivitySummary.style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 const ActivitySummary = styled.section`
   border-bottom: 1px solid lightgrey;
   margin: 0 auto;
-  padding: 2em 1em 2em 1em;
+  padding: 1em 1em 2em 1em;
 
   .activity-main-container {
     display: flex;
