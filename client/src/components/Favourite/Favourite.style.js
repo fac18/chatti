08683d1fe1b52aebd2a1556698f09da89b11d978
Gillian <gillian@yeomans.org.uk>
2020-02-20 19:04:07 +0000

    update favourites

--- client/src/components/Favourite/Favourite.style.js
+++ client/src/components/Favourite/Favourite.style.js
@@ -1,22 +1,31 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Favourite = styled.section`
   display: flex;
   flex-direction: row;
 
+  .activity-preview-container {
+    object-fit: cover;
+    width: 1000%;
+    margin-left: 0.5rem;
+    display: flex;
+    align-items: center;
+  }
+
   .activity-preview {
-    width: 50%;
+    width: 100%;
   }
 
   .right-container {
     display: flex;
     flex-direction: column;
+    margin: 1rem;
   }
 
   .top-container {
     display: flex;
     flex-direction: row;
   }
-`;
+`
 
-export { Favourite };
+export { Favourite }
