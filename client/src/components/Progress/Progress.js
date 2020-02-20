95c7b756187ba4126cd5cdfe6c1c6448276f0011
Pat Cichocki <pat.cichocki@gmail.com>
2020-02-20 11:35:16 +0000

    progress page initial styling

--- client/src/components/Progress/Progress.js
+++ client/src/components/Progress/Progress.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../Header/Header'
 import Navbar from '../Navbar/Navbar'
 import calcProgressStats from '../../utils/calcProgressStats'
+import { ProgressContainer } from '../masterCss'
+
 
 function Progress({ userData, userLibrary }) {
   const [progressStats, setProgressStats] = React.useState(null)
@@ -17,12 +19,22 @@ function Progress({ userData, userLibrary }) {
       {progressStats && (
         <>
           <Header buttons />
-          <h1>Your Progress</h1>
-          <h3>Total Chatti time:</h3>
-          <h2>{progressStats.minutesCompleted}</h2>
-          <h3>Activities completed:</h3>
-          <h2>{progressStats.activitiesCompleted}</h2>
-          <h3>Your badges:</h3>
+          <ProgressContainer>
+                      
+            <h1>Your Progress</h1>
+            <div className="progress-section">
+              <h3>Total Chatti time:</h3>
+              <h2>{progressStats.minutesCompleted}</h2>
+            </div>
+            <div className="progress-section">
+              <h3>Activities completed:</h3>
+              <h2>{progressStats.activitiesCompleted}</h2>
+            </div>
+            <div className="progress-section">
+              <h3>Your badges:</h3>
+            </div>
+
+          </ProgressContainer>
           {/* badges here */}
           <Navbar />
         </>
